4ab2645da36af24bd536c994b2effe697e3932d2
Anton Weber <anton@antweb.me>
2020-03-21 17:51:45 +0100

    Fixed formatting.

--- app/app.py
+++ app/app.py
@@ -1,23 +1,24 @@
 from flask import Flask, request
+
 app = Flask(__name__)
 
 
-@app.route("/user", methods = ["POST"])
+@app.route("/user", methods=["POST"])
 def create_user():
     print(request.headers)
 
 
-@app.route("/murmel", methods = ["POST"])
+@app.route("/murmel", methods=["POST"])
 def create_murmel():
     print(request.headers)
 
 
-@app.route("/murmel/me/current", methods = ["GET"])
+@app.route("/murmel/me/current", methods=["GET"])
 def get_current_murmel():
     print(request.headers)
 
 
-@app.route("/murmel/radar?location=Foo", methods = ["GET"])
+@app.route("/murmel/radar?location=Foo", methods=["GET"])
 def get_murmel_radar():
     print(request.headers)
 
