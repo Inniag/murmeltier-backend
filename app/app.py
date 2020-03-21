f79680d535fb1a6e7f03001f65a1d004519059b9
Gianni Klesse <gianni.klesse@gmx.de>
2020-03-21 17:48:16 +0100

    Fixed print statements.

--- app/app.py
+++ app/app.py
@@ -4,22 +4,22 @@ app = Flask(__name__)
 
 @app.route("/user", methods = ["POST"])
 def create_user():
-    print request.headers
+    print(request.headers)
 
 
 @app.route("/murmel", methods = ["POST"])
 def create_murmel():
-    print request.headers
+    print(request.headers)
 
 
 @app.route("/murmel/me/current", methods = ["GET"])
 def get_current_murmel():
-    print request.headers
+    print(request.headers)
 
 
 @app.route("/murmel/radar?location=Foo", methods = ["GET"])
 def get_murmel_radar():
-    print request.headers
+    print(request.headers)
 
 
 if __name__ == "__main__":
